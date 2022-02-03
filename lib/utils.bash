db5fa342e5e2ec1e1989e91c9e5127c81c238d82
William Poetra Yoga <william.poetra@gmail.com>
2022-02-03 14:12:02 +0700

    feat: adapt download logic

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for openvscode-server.
 GH_REPO="https://github.com/gitpod-io/openvscode-server"
 TOOL_NAME="openvscode-server"
 TOOL_TEST="openvscode-server --version"
@@ -31,18 +30,48 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if openvscode-server has other means of determining installable versions.
-  list_github_tags
+  #
+  # Old release tags start with a number like '1.48.2' and don't contain prebuilt binaries
+  #
+  # Starting from version 1.60.0, release tags look like 'openvscode-server-v1.60.0':
+  # - prebuilt binaries are provided
+  # - bin/openvscode-server needs to be replaced with server.sh
+  # - after replacing, bin/openvscode-server needs to be adjusted (for the location change)
+  #
+  # Starting from version 1.64.0 (right now still an insiders build):
+  # - bin/openvscode-server is ready to use out of the box
+  # - server.sh has been removed
+  #
+  list_github_tags | grep 'openvscode-server-v[0-9]'
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+  local version="$1"
+  local filename="$2"
 
-  # TODO: Adapt the release URL convention for openvscode-server
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  local uname_s="$(uname -s)"
+  local uname_m="$(uname -m)"
+  local os arch
+
+  case "$uname_s" in
+  # FreeBSD) os="freebsd" ;;
+  # Darwin) os="darwin" ;;
+  Linux) os="linux" ;;
+  *) fail "OS not supported: $uname_s" ;;
+  esac
+
+  case "$uname_m" in
+  # i?86) arch="386" ;;
+  x86_64) arch="x64" ;;
+  aarch64) arch="arm64" ;;
+  armv8l) arch="arm64" ;;
+  arm64) arch="arm64" ;;
+  armv7l) arch="armhf" ;;
+  *) fail "Architecture not supported: $uname_m" ;;
+  esac
+
+  local url="$GH_REPO/releases/download/openvscode-server-v${version}/openvscode-server-v${version}-${os}-${arch}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +90,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert openvscode-server executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
